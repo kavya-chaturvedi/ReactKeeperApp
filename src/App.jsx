a0b12c7912e68876b73b7d30594585d59b438008
kavya-chaturvedi <70815337+kavya-chaturvedi@users.noreply.github.com>
2021-01-18 01:08:43 +0530

    Note is added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <div>
       <Heading />
+      <Note />
       <Footer />
     </div>
   );
---
+++ src/Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  var date = new Date();
+  var currentYear = date.getFullYear();
+
+  return (
+    <div>
+      <h1>This is Content Title</h1>
+      <p>This is Content Paragraph</p>
+    </div>
+  );
+}
+
+export default Note;
